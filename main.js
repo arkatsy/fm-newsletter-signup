52af0b1caa34ca6061a023adb1507ee8262be429
Argyris <arkatsy@gmail.com>
2024-04-25 07:06:10 +0300

    feat: add dialog

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/700.css";
+
+const dialog = document.getElementById("success-dialog");
+const input = document.getElementById("email");
+const subscribeBtn = document.getElementById("subscribe-btn");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  dialog.showModal();
+  addInfoToModal(input.value);
+});
+
+function addInfoToModal(email) {
+  const insertEmailNode = document.getElementById("insert-email");
+  insertEmailNode.textContent = email;
+}
